ae7327cd71b68169a7db0878bf28e5bc90344a08
Aadish Jain <aadishhere@gmail.com>
2025-05-14 12:01:47 +0530

    Initial Commit

---
+++ topBarMenuWith3Pages/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  topBarMenuWith3Pages
+//
+//  Created by Aadish Jain on 14/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ topBarMenuWith3Pages/topBarMenuWith3PagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  topBarMenuWith3PagesApp.swift
+//  topBarMenuWith3Pages
+//
+//  Created by Aadish Jain on 14/05/25.
+//
+
+import SwiftUI
+
+@main
+struct topBarMenuWith3PagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
